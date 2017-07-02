8de73e87658ac1e16fb864f8b62f226851c16515
Jack Sarick <jacksarick@gmail.com>
2017-07-01 20:57:16 -0600

    Condensed your logic
    Most if, else statements that "sort" things can be condensed to a single line with some arrays

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 function Qmarkup(tag){
 	this.build = function(text, flags){
-		let ids = ""
-		let cls = ""
+		let modifiers = {"#": [], ".": []}
 
 		if (typeof text == "object") {
 			text.toString()
@@ -9,21 +8,11 @@ function Qmarkup(tag){
 
 		if (typeof flags == "object"){
 			for (var i = 0; i < flags.length; i++) {
-				if (flags[i][0] == ".") {
-					cls += flags[i].slice(1) + " "
-				}
-				else if (flags[i][0] == "#") {
-					ids += flags[i].slice(1) + " "
-				}
+				modifiers[flags[0]] += flags.slice(1) + " "
 			}
 		}
 		else if (typeof flags == "string") {
-			if (flags[0] == ".") {
-				cls += flags.slice(1)
-			}
-			else if (flags[0] == "#") {
-				ids += flags.slice(1)
-			}
+			modifiers[flags[0]] += flags.slice(1)
 		}
 		return `<${tag} id="${ids}" class="${cls}">${text}</${tag}>`
 	}
